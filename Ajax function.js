5e65259428c704b997bf273c26681f5a08112601
天儿 <34054179+Singleangel233@users.noreply.github.com>
2017-11-28 16:02:01 +0800

    Add files via upload

---
+++ Ajax function.js
@@ -0,0 +1,26 @@
+function ajax(option){
+	var xhr = new XMLHttpRequest();
+	if(option.type == 'get' && option.data){
+			option.url += '?';
+			option.url += option.data;
+			option.data = null;
+	}
+	if(option.type == 'post' && option.data){
+		xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
+	}
+	xhr.onreadystatechange(){
+		if(xhr.readyState == 4 && xhr.status == 200){
+			var filetype = xhr.getResponseHeader('Content-Type');
+			if(filetype.indexof('json') != -1){
+				option.success(JSON.parse(xhr.responseText));
+			}
+			if (filetype.indexof('xml') != -1){
+				option.success(xhr.responseXML);
+			}
+			else{
+				option.success(xhr.responseText);
+			}
+		}
+	}
+	xhr.send(option.data);
+}
