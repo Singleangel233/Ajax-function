35d3ce633f9d5fc83fba7c1ac62d78b93ea3fa60
singleangel233 <740518644@qq.com>
2017-11-29 21:50:37 +0800

    Revert "Add files via upload"
    This reverts commit 5e65259428c704b997bf273c26681f5a08112601.

--- Ajax function.js
+++ Ajax function.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 function ajax(option){
 	var xhr = new XMLHttpRequest();
 	if(option.type == 'get' && option.data){
@@ -25,58 +23,4 @@ function ajax(option){
 		}
 	}
 	xhr.send(option.data);
-=======
-function ajax(option){
-	var xhr = new XMLHttpRequest();
-	if(option.type == 'get' && option.data){
-			option.url += '?';
-			option.url += option.data;
-			option.data = null;
-	}
-	if(option.type == 'post' && option.data){
-		xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-	}
-	xhr.onreadystatechange(){
-		if(xhr.readyState == 4 && xhr.status == 200){
-			var filetype = xhr.getResponseHeader('Content-Type');
-			if(filetype.indexof('json') != -1){
-				option.success(JSON.parse(xhr.responseText));
-			}
-			if (filetype.indexof('xml') != -1){
-				option.success(xhr.responseXML);
-			}
-			else{
-				option.success(xhr.responseText);
-			}
-		}
-	}
-	xhr.send(option.data);
->>>>>>> 1865f97c6ac741e826bde0cc0e98709bf0177de2
-=======
-function ajax(option){
-	var xhr = new XMLHttpRequest();
-	if(option.type == 'get' && option.data){
-			option.url += '?';
-			option.url += option.data;
-			option.data = null;
-	}
-	if(option.type == 'post' && option.data){
-		xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-	}
-	xhr.onreadystatechange(){
-		if(xhr.readyState == 4 && xhr.status == 200){
-			var filetype = xhr.getResponseHeader('Content-Type');
-			if(filetype.indexof('json') != -1){
-				option.success(JSON.parse(xhr.responseText));
-			}
-			if (filetype.indexof('xml') != -1){
-				option.success(xhr.responseXML);
-			}
-			else{
-				option.success(xhr.responseText);
-			}
-		}
-	}
-	xhr.send(option.data);
->>>>>>> 1865f97c6ac741e826bde0cc0e98709bf0177de2
 }
